8e9b6f3ca5fea76dbcfede5c96f5893d749ee0a7
Riel42 <110313438+Riel42@users.noreply.github.com>
2023-04-04 12:32:22 -0300

    Create Boolean.cpp

---
+++ archives/Boolean.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int sol = 0;
+    
+    if(sol){
+        cout << "Vou ao cinema!\n"; 
+    }
+    else{
+        cout << "Vou ao clube!\n"; //vai compilar esse, porque 0 é falso
+    }
+}
